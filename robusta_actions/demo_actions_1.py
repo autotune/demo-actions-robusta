5324b38acc30537d722f6e3018f00a224b66e589
ExperiencedDevOps <service@contrasting.org>
2022-10-08 16:53:40 -0500

    Update demo_actions_1.py

--- robusta_actions/demo_actions_1.py
+++ robusta_actions/demo_actions_1.py
@@ -7,8 +7,8 @@ def report_scheduling_failure(event: EventEvent): # We use EventEvent to get the
     print(f"This print will be shown in the robusta logs={actual_event}")
 
     if actual_event.type.casefold() == f'Normal'.casefold() and \
-        actual_event.reason.casefold() == f'ScalingReplicaSet'.casefold()
-    _report_failed_scheduling(event, actual_event.involvedObject.name, actual_event.message)
+        actual_event.reason.casefold() == f'ScalingReplicaSet'.casefold():
+        _report_failed_scheduling(event, actual_event.involvedObject.name, actual_event.message)
 
 def _report_failed_scheduling(event: EventEvent, pod_name: str, message: str):
     custom_message = ""
