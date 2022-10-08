1d9a7cbfa3f9b7f192c281636830e3ac11bd1c6d
ExperiencedDevOps <service@contrasting.org>
2022-10-08 17:10:07 -0500

    Update demo_actions_1.py

--- robusta_actions/demo_actions_1.py
+++ robusta_actions/demo_actions_1.py
@@ -8,18 +8,18 @@ def report_scheduling_failure(event: ReplicaSetEvent): # We use ReplicaSetEvent 
     print(f"This print will be shown in the robusta logs={actual_event}")
 
     if actual_event.type.casefold() == f'Normal'.casefold() and \
-        actual_event.reason.casefold() == f'ScalingReplicaSet'.casefold():
-        _report_failed_scheduling(event, actual_event.involvedObject.name, actual_event.message)
+        # actual_event.reason.casefold() == f'ScalingReplicaSet'.casefold():
+        _report_succeded_scheduling(event, actual_event.involvedObject.name, actual_event.message)
 
-def _report_failed_scheduling(event: EventEvent, pod_name: str, message: str):
+def _report_succeded_scheduling(event: EventEvent, pod_name: str, message: str):
     custom_message = ""
-    if "affinity/selector" in message:
-        custom_message = "Your pod has a node 'selector' configured, which means it can't just run on any node. For more info, see: https://kubernetes.io/docs/concepts/scheduling-eviction/assign-pod-node/#nodeselector"
+    # if "affinity/selector" in message:
+    #     custom_message = "Your pod has a node 'selector' configured, which means it can't just run on any node. For more info, see: https://kubernetes.io/docs/concepts/scheduling-eviction/assign-pod-node/#nodeselector"
 
     # this is how you send data to slack or other destinations
 
     # Note - is it sometimes better to create a Finding object instead of calling event.add_enrichment, but this is out of the scope of this tutorial
 
     event.add_enrichment([
-        MarkdownBlock(f"Failed to schedule a pod named '{pod_name}'!\nerror: {message}\n\n{custom_message}"),
+        MarkdownBlock(f"Succeeded to schedule a pod named '{pod_name}'!\nerror: {message}\n\n{custom_message}"),
     ])
