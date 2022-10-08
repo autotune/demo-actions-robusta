14874a29f4c0c1c0ac7df6e28360019b4e038bb6
ExperiencedDevOps <service@contrasting.org>
2022-10-08 17:28:04 -0500

    Update demo_actions_1.py

--- robusta_actions/demo_actions_1.py
+++ robusta_actions/demo_actions_1.py
@@ -2,14 +2,14 @@ from robusta.api import *
 
 # see: https://docs.robusta.dev/master/developer-guide/actions/triggers-and-events.html#events-and-triggers
 @action
-def report_scheduling_failure(event: ReplicaSetEvent): # We use ReplicaSetEvent to get the event object.
+def report_scheduling_succeded(event: ReplicaSetEvent): # We use ReplicaSetEvent to get the event object.
     actual_event = event.get_event()
 
     print(f"This print will be shown in the robusta logs={actual_event}")
 
-    if actual_event.type.casefold() == f'Normal'.casefold():
+    # if actual_event.type.casefold() == f'Normal'.casefold():
         # actual_event.reason.casefold() == f'ScalingReplicaSet'.casefold():
-        _report_succeded_scheduling(event, actual_event.involvedObject.name, actual_event.message)
+    _report_succeded_scheduling(event, actual_event.involvedObject.name, actual_event.message)
 
 def _report_succeded_scheduling(event: ReplicaSetEvent, ObjName: str, message: str):
     custom_message = ""
